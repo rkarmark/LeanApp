87c9f48402b0f6fda302a06f4ca855f8712354df
rkarmark <rkarmark@outlook.dk>
2023-10-22 21:32:31 +0200

    updated db util

--- server/utils/db.ts
+++ server/utils/db.ts
@@ -1,18 +1,11 @@
 import { drizzle, LibSQLDatabase } from "drizzle-orm/libsql";
 import { createClient } from "@libsql/client";
 
-let db: LibSQLDatabase
-
-try {
-  if(process.env.DATABASE_URL && process.env.DATABASE_TOKEN){
-    db = drizzle(createClient({
-    url: process.env.DATABASE_URL,
-    authToken: process.env.DATABASE_TOKEN,
-    }
-  ))};
-} catch {
-  throw new Error("DATABASE_URL and DATABASE_TOKEN must be set in .env");
-}
-
 export * as tables from "~/server/db/schema";
-export const useDB = () => db
+export const useDB = () =>
+  drizzle(
+    createClient({
+      url: process.env.DATABASE_URL,
+      authToken: process.env.DATABASE_TOKEN,
+    }),
+  );
